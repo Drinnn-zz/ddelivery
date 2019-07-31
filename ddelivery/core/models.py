980c13097c52aea6d93e928718edb994d884e13e
Drinnn <pedro_lontro@hotmail.com>
2019-07-30 22:22:28 -0300

    Criação dos migrations iniciais

--- ddelivery/core/models.py
+++ ddelivery/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class MalhaFerroviaria(models.Model):
+    ponto_origem: models.CharField(max_length=2)
+    ponto_destino: models.CharField(max_length=2)
+
+
+class Mapa(models.Model):
+    nome: models.CharField(max_length=100)
+    id_malha_ferrov: models.ForeignKey(
+        MalhaFerroviaria, on_delete=models.CASCADE)
